31bc63481726d3abff4891d6e0489277ce2db074
Charly <cramirezdiaz.dev@gmail.com>
2026-06-14 11:23:38 -0300

    redicrección

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://gasistasrosario.com.ar',
   trailingSlash: 'never',
+  build: {
+    format: 'file',
+  },
   integrations: [
     tailwind(),
     sitemap(),
